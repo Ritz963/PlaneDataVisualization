75d75a9f8d1e6a3c12653d353f457eb0e1b3371d
ritvik-T <ritvik.thakur04@gmail.com>
2023-09-20 16:24:59 -0400

    9/20

--- StrikeVisual.py
+++ StrikeVisual.py
@@ -3,39 +3,43 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('Airplane_Strikes_Dataset.csv')
 
+##### Display where strikes occured most often #####
+
+# locations = ['Radome Strike' , 'Windshield Strike' , 'Nose Strike' , 'Engine1 Strike' , 'Engine2 Strike' , 'Engine3 Strike' , 'Engine4 Strike' , 'Propeller Strike' , 'Wing or Rotor Strike' , 'Fuselage Strike' , 'Landing Gear Strike' , 'Tail Strike' , 'Lights Strike' , 'Other Strike']
+# locationSum = df[locations].sum
+# plt.figure(figsize=(12, 6))  # Adjust the figure size as needed
+
+# locationSum = pd.Series(locationSum)
+
+
+# locationSum.plot(kind = 'b ar')
+# plt.title('Location strike frequency')
+# plt.xlabel('Locations')
+# plt.ylabel('Frequency')
+# plt.xticks(rotation=45)
+
+
 ##### Displays strike frequency each year #####
-YearColumn = df['Incident Year']
-number_counts = YearColumn.value_counts()
+# YearColumn = df['Incident Year']
+# number_counts = YearColumn.value_counts()
 
-number_counts_sorted= number_counts.sort_index()
+# number_counts_sorted= number_counts.sort_index()
 
-plt.rcParams['figure.figsize'] = (12, 6)
+# plt.rcParams['figure.figsize'] = (12, 6)
 
-fig, ax = plt.subplots()
+# fig, ax = plt.subplots()
 
-number_counts_sorted.plot(kind='bar')
+# number_counts_sorted.plot(kind='bar')
 
-# bar_color = color
-# for bar in bars:
-#   ax.text(
-#       bar.get_x() + bar.get_width() / 2,
-#       bar.get_height() + 0.3,
-#       round(bar.get_height(), 1),
-#       horizontalalignment='center',
-#       color=bar_color,
-#       weight='bold'
-#   )
+# plt.title('Strike Frequency each year')
 
+# plt.xlabel('Year')
+# plt.ylabel('Strike Frequency')
 
-plt.title('Strike Frequency each year')
+# for spine in ['top', 'right']:
+#     ax.spines[spine].set_visible(False)
 
-plt.xlabel('Year')
-plt.ylabel('Strike Frequency')
-
-for spine in ['top', 'right']:
-    ax.spines[spine].set_visible(False)
-
-fig.tight_layout()
+# fig.tight_layout()
 
 
 
@@ -53,6 +57,7 @@ fig.tight_layout()
 # plt.title('Engine Type Strike Frequency')
 # plt.xlabel('Engine Type')
 # plt.ylabel('Frequency')
+# ax.set_xticklabels(ax.get_xticks(), rotation=90)
 
 
 
